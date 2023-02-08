b0c759cfc54d0b6c0ab1743da11e7c242a558d7f
Xinlyu Wang <xinlyuwang96@gmail.com>
2023-02-08 08:05:51 +0800

    add backend rooutes and POST in elec page

--- ecotrack-backend/server.py
+++ ecotrack-backend/server.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request,jsonify, json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/members')
-def hello_world():
-    return {"members": ["A", "B", "C"]}
+
+@app.route('/elecdata', methods=['POST'])
+def add_elecdata():
+    data = request.get_json()
+    
+    return jsonify(data)
+
+@app.route('/data')
+def send_data():
+    data = {
+        "key1": 2,
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
