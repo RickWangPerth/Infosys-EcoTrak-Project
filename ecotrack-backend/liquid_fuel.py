da488d601d1afaa6e8734da20505fb559a697248
Marshall <mpoon@melbournespace.com.au>
2023-02-13 10:26:59 +1100

    Liquid fuel for kL

--- ecotrack-backend/liquid_fuel.py
+++ ecotrack-backend/liquid_fuel.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 parameters = pd.read_excel('./Documentation/Emission_Source_Parameters_Data.xlsx',
-                           sheet_name=['Electricity', 'Solid Fuel', 'Gaseous Fuel', 'Natural Gas Data', 'Liquid Fuel', 'Solid Waste', 'Wastewater Treatment', 'Waste Incineration', 'Biological Treatment'])
+                           sheet_name=['Electricity', 'Solid Fuel', 'Gaseous Fuel', 'Natural Gas Data', 'Liquid Fuel kL', 'Liquid Fuel t', 'Solid Waste', 'Wastewater Treatment', 'Waste Incineration', 'Biological Treatment'])
 
-liquid_fuel_parameters = parameters.get('Liquid Fuel')
+liquid_fuel_kL_parameters = parameters.get('Liquid Fuel kL')
+liquid_fuel_t_parameters = parameters.get('Liquid Fuel t')
 
 # INPUT FROM FRONT END - Quantity of Fuel Type in tonnes
 Q = 700
@@ -15,19 +16,19 @@ unit = 'kL'
 def liquid(Q, type, unit):
     for i in range(20):
 
-        if liquid_fuel_parameters.iloc[i][0] == type:
+        if liquid_fuel_kL_parameters.iloc[i][0] == type:
             if unit == 'GJ':
                 EC = 1
             else:
-                EC = liquid_fuel_parameters.iloc[i][1]
+                EC = liquid_fuel_kL_parameters.iloc[i][1]
 
             # Scope 1 Emission Factors for CO2, CH4 and N2O
-            EF_1_CO2 = liquid_fuel_parameters.iloc[i][2]
-            EF_1_CH4 = liquid_fuel_parameters.iloc[i][3]
-            EF_1_N2O = liquid_fuel_parameters.iloc[i][4]
+            EF_1_CO2 = liquid_fuel_kL_parameters.iloc[i][2]
+            EF_1_CH4 = liquid_fuel_kL_parameters.iloc[i][3]
+            EF_1_N2O = liquid_fuel_kL_parameters.iloc[i][4]
             total_EF_1 = EF_1_CO2 + EF_1_CH4 + EF_1_N2O
             # Scope 3 Emission Factor
-            EF_3 = liquid_fuel_parameters.iloc[i][6]
+            EF_3 = liquid_fuel_kL_parameters.iloc[i][6]
 
     # Scope 1 Emission
     CO2_e = float(Q) * EC * (EF_1_CO2) / 1000
