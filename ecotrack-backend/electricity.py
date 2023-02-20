ea97b61c88abe097df51408dc19615c9e57874f0
Marshall <mpoon@melbournespace.com.au>
2023-02-20 11:56:30 +1100

    first commit of electricity

--- ecotrack-backend/electricity.py
+++ ecotrack-backend/electricity.py
@@ -1,26 +1,78 @@
+import psycopg2
 import pandas as pd
 
-parameters = pd.read_excel('./Documentation/Emission_Source_Parameters_Data.xlsx',
-                           sheet_name=['Electricity', 'Solid Waste', 'Wastewater Treatment', 'Waste Incineration', 'Biological Treatment'])
 
-electricity_parameters = parameters.get('Electricity')
-# Emissions based on electricity from electrical grid
+# Connection parameters
+param_dic = {
+    "host": "localhost",
+    "database": "emissions",
+    "user": "postgres",
+    "password": "pB1@ckburn"
+}
 
-# Q_elec = 11300000  # INPUT FROM FRONT END - ENERGY USAGE
-# state = 'National'       # INPUT FROM FRONT END - STATE
-# unit = 'kWh'
 
-def elecal(state,unit,Q_elec):
+def connect(params_dic):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params_dic)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        sys.exit(1)
+    print("Connection successful")
+    return conn
+
+
+def postgresql_to_dataframe(conn, select_query, column_names):
+    """
+    Tranform a SELECT query into a pandas dataframe 
+    """
+    cursor = conn.cursor()
+    try:
+        cursor.execute(select_query)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        cursor.close()
+        return 1
+
+    # Naturally we get a list of tupples
+    tupples = cursor.fetchall()
+    cursor.close()
+
+    # We just need to turn it into a pandas dataframe
+    df = pd.DataFrame(tupples, columns=column_names)
+    return df
+
+
+conn = connect(param_dic)
+column_names = ["id", "sector", "state", "sc2", "sc3", "unit"]
+# Execute the "SELECT *" query
+df = postgresql_to_dataframe(
+    conn, "select * from electricity_ref", column_names)
+df.head()
+
+
+def elecal(state, unit, Q_elec):
     for i in range(12):
         if unit == 'kWh':
-            if electricity_parameters.iloc[i][0] == state:
-                EF_2 = electricity_parameters.iloc[i][1]
-                EF_3 = electricity_parameters.iloc[i][3]
+            if df.iloc[i][0] == state:
+                EF_2 = df.iloc[i][1]
+                EF_3 = df.iloc[i][3]
                 print(state)
         if unit == 'GJ':
-            if electricity_parameters.iloc[i][0] == state:
-                EF_2 = electricity_parameters.iloc[i][2]
-                EF_3 = electricity_parameters.iloc[i][4]
+            if df.iloc[i][0] == state:
+                EF_2 = df.iloc[i][2]
+                EF_3 = df.iloc[i][4]
                 print(state)
-    elec_e =  float(Q_elec) * (EF_2 + EF_3) / 1000
-    return elec_e 
+    elec_e = float(Q_elec) * (EF_2 + EF_3) / 1000
+    return elec_e
+
+
+Q_elec = 11300000  # INPUT FROM FRONT END - ENERGY USAGE
+state = 'National'       # INPUT FROM FRONT END - STATE
+unit = 'kWh'
+
+emissions = elecal(state, unit, Q_elec)
+print(emissions)
