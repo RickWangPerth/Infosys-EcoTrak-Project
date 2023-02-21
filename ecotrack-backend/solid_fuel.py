018476e6026c97db499705e54a22b8a3cfe8967e
Marshall <mpoon@melbournespace.com.au>
2023-02-21 15:56:28 +1100

    First commit of solid fuel with database parameters

--- ecotrack-backend/solid_fuel.py
+++ ecotrack-backend/solid_fuel.py
@@ -1,43 +1,97 @@
+import psycopg2
 import pandas as pd
 
-parameters = pd.read_excel('./Documentation/Emission_Source_Parameters_Data.xlsx',
-                           sheet_name=['Electricity', 'Solid Fuel', 'Gaseous Fuel', 'Natural Gas Data', 'Liquid Fuel kL', 'Liquid Fuel t', 'Solid Waste', 'Wastewater Treatment', 'Waste Incineration', 'Biological Treatment'])
+host = 'localhost'
+database = 'ecotrak'
+user = 'postgres'
+password = ''
+table_name = 'fuels_ef'
+column_names = ['id', 'sector', 'subsector', 'type', 'ratio',
+                'unit', 'sc1_co2', 'sc1_ch4', 'sc1_n20', 'sc1_sum', 'sc3_ef']
+
+# Connection parameters
+param_dic = {
+    "host": host,
+    "database": database,
+    "user": user,
+    "password": password
+}
 
 
-solid_fuel_parameters = parameters.get('Solid Fuel')
+def connect(params_dic):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params_dic)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        sys.exit(1)
+    print("Connection successful")
+    return conn
+
+
+def postgresql_to_dataframe(conn, select_query, column_names):
+    """
+    Tranform a SELECT query into a pandas dataframe
+    """
+    cursor = conn.cursor()
+    try:
+        cursor.execute(select_query)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        cursor.close()
+        return 1
+
+    # Naturally we get a list of tupples
+    tupples = cursor.fetchall()
+    cursor.close()
+
+    # We just need to turn it into a pandas dataframe
+    df = pd.DataFrame(tupples, columns=column_names)
+    return df
+
+
+conn = connect(param_dic)
+column_names = column_names
+# Execute the "SELECT *" query
+df = postgresql_to_dataframe(
+    conn, "select * from " + table_name, column_names)
+df.head()
+
 
 # INPUT FROM FRONT END - Quantity of Fuel Type in tonnes
 Q = 20000
 # INPUT FROM FRONT END - Type of fuel
 type = 'Brown coal (lignite)'
+subsector = 'Solid Fuel'
+
+subsector_df = df.loc[df['subsector'] == 'Solid Fuel']
 
 
 def solidfuelcal(Q, type):
-    for i in range(20):
+    EC = subsector_df.loc[subsector_df['type']
+                          == type, 'ratio'].iloc[0]
+    sc1_co2 = subsector_df.loc[subsector_df['type']
+                               == type, 'sc1_co2'].iloc[0]
+    sc1_ch4 = subsector_df.loc[subsector_df['type']
+                               == type, 'sc1_ch4'].iloc[0]
+    sc1_n20 = subsector_df.loc[subsector_df['type']
+                               == type, 'sc1_n20'].iloc[0]
+    sc1_sum = subsector_df.loc[subsector_df['type']
+                               == type, 'sc1_sum'].iloc[0]
+    sc3_ef = subsector_df.loc[subsector_df['type']
+                              == type, 'sc3_ef'].iloc[0]
 
-        if solid_fuel_parameters.iloc[i][0] == type:
-            EC = solid_fuel_parameters.iloc[i][1]
+    CO2_e = float(Q) * EC * (sc1_co2) / 1000
+    CH4_e = float(Q) * EC * (sc1_ch4) / 1000
+    N2O_e = float(Q) * EC * (sc1_n20) / 1000
 
-            # Scope 1 Emission Factors for CO2, CH4 and N2O
-            EF_1_CO2 = solid_fuel_parameters.iloc[i][2]
-            EF_1_CH4 = solid_fuel_parameters.iloc[i][3]
-            EF_1_N2O = solid_fuel_parameters.iloc[i][4]
-            total_EF_1 = EF_1_CO2 + EF_1_CH4 + EF_1_N2O
-            # Scope 3 Emission Factor
-            EF_3 = solid_fuel_parameters.iloc[i][6]
+    total_e = float(Q) * EC * (sc1_sum + sc3_ef) / 1000
 
-    # Scope 1 Emission
-    CO2_e = float(Q) * EC * (EF_1_CO2) / 1000
-    CH4_e = float(Q) * EC * (EF_1_CH4) / 1000
-    N2O_e = float(Q) * EC * (EF_1_N2O) / 1000
-    # Scope 3 Emission
-    scope_3_e = float(Q) * EC * (EF_3) / 1000
-    # Total Combined Emissions
-    total_e = float(Q) * EC * (total_EF_1 + EF_3) / 1000
-
-    # return total_e, CO2_e, CH4_e, N2O_e, scope_3_e
-    return total_e
+    return total_e, CO2_e, CH4_e, N2O_e
 
 
-# total = solid(Q, 'Brown coal (lignite)')
-# print(total)
+total = solidfuelcal(Q, type)
+print(total)
