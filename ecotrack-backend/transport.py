7cfa349464e23696c365d600b41f05f081cfd7f6
Marshall <mpoon@melbournespace.com.au>
2023-02-27 12:32:09 +1100

    Transport functions added

--- ecotrack-backend/transport.py
+++ ecotrack-backend/transport.py
@@ -2,12 +2,12 @@ import psycopg2
 import pandas as pd
 
 host = 'localhost'
-database = 'ecotrak'
+database = 'ecotrakv2'
 user = 'postgres'
-password = ''
-table_name = 'fuels_ef'
-column_names = ['id', 'sector', 'subsector', 'type', 'ratio',
-                'unit', 'sc1_co2', 'sc1_ch4', 'sc1_n20', 'sc1_sum', 'sc3_ef']
+password = 'pB1@ckburn'
+table_name = 'transport_ef'
+column_names = ['id', 'transport_type', 'fuel_type', 'unit', 'ratioGJperKL',
+                'sc1_co2', 'sc1_ch4', 'sc1_n20', 'sc1_sum', 'sc3_ef']
 
 # Connection parameters
 param_dic = {
@@ -57,3 +57,110 @@ column_names = column_names
 df = postgresql_to_dataframe(
     conn, "select * from " + table_name, column_names)
 df.head()
+
+
+Q = 1150
+transport_type = 'Cars and light commercial vehicles'
+fuel_type = 'Gasoline'
+unit = 'kL'
+
+cars_df = df.loc[df['transport_type'] == 'Cars and light commercial vehicles']
+heavy_df = df.loc[df['transport_type'] == 'Heavy duty vehicles']
+light_df = df.loc[df['transport_type'] == 'Light duty vehicles']
+aviation_df = df.loc[df['transport_type'] == 'Aviation']
+
+
+def cars(Q, fuel_type, unit):
+    if unit == 'GJ':
+        EC = 1
+    else:
+        EC = cars_df.loc[df['fuel_type']
+                         == fuel_type, 'ratioGJperKL'].iloc[0]
+
+    sc1_co2 = cars_df.loc[df['fuel_type']
+                          == fuel_type, 'sc1_co2'].iloc[0]
+    sc1_ch4 = cars_df.loc[df['fuel_type']
+                          == fuel_type, 'sc1_ch4'].iloc[0]
+    sc1_n20 = cars_df.loc[df['fuel_type']
+                          == fuel_type, 'sc1_n20'].iloc[0]
+    sc1_sum = cars_df.loc[df['fuel_type']
+                          == fuel_type, 'sc1_sum'].iloc[0]
+
+    CO2_e = float(Q) * EC * (sc1_co2) / 1000
+    CH4_e = float(Q) * EC * (sc1_ch4) / 1000
+    N2O_e = float(Q) * EC * (sc1_n20) / 1000
+    total_e = float(Q) * EC * (sc1_sum) / 1000
+
+    return total_e, CO2_e, CH4_e, N2O_e
+
+
+def heavy(Q, fuel_type, unit):
+    if unit == 'GJ':
+        EC = 1
+    else:
+        EC = heavy_df.loc[df['fuel_type']
+                          == fuel_type, 'ratioGJperKL'].iloc[0]
+
+    sc1_co2 = heavy_df.loc[df['fuel_type']
+                           == fuel_type, 'sc1_co2'].iloc[0]
+    sc1_ch4 = heavy_df.loc[df['fuel_type']
+                           == fuel_type, 'sc1_ch4'].iloc[0]
+    sc1_n20 = heavy_df.loc[df['fuel_type']
+                           == fuel_type, 'sc1_n20'].iloc[0]
+    sc1_sum = heavy_df.loc[df['fuel_type']
+                           == fuel_type, 'sc1_sum'].iloc[0]
+
+    CO2_e = float(Q) * EC * (sc1_co2) / 1000
+    CH4_e = float(Q) * EC * (sc1_ch4) / 1000
+    N2O_e = float(Q) * EC * (sc1_n20) / 1000
+    total_e = float(Q) * EC * (sc1_sum) / 1000
+
+    return total_e, CO2_e, CH4_e,
+
+
+def light(Q, fuel_type, unit):
+    if unit == 'GJ':
+        EC = 1
+    else:
+        EC = light_df.loc[df['fuel_type']
+                          == fuel_type, 'ratioGJperKL'].iloc[0]
+
+    sc1_co2 = light_df.loc[df['fuel_type']
+                           == fuel_type, 'sc1_co2'].iloc[0]
+    sc1_ch4 = light_df.loc[df['fuel_type']
+                           == fuel_type, 'sc1_ch4'].iloc[0]
+    sc1_n20 = light_df.loc[df['fuel_type']
+                           == fuel_type, 'sc1_n20'].iloc[0]
+    sc1_sum = light_df.loc[df['fuel_type']
+                           == fuel_type, 'sc1_sum'].iloc[0]
+
+    CO2_e = float(Q) * EC * (sc1_co2) / 1000
+    CH4_e = float(Q) * EC * (sc1_ch4) / 1000
+    N2O_e = float(Q) * EC * (sc1_n20) / 1000
+    total_e = float(Q) * EC * (sc1_sum) / 1000
+
+    return total_e, CO2_e, CH4_e, N2O_e
+
+
+def aviation(Q, fuel_type, unit):
+    if unit == 'GJ':
+        EC = 1
+    else:
+        EC = aviation_df.loc[df['fuel_type']
+                             == fuel_type, 'ratioGJperKL'].iloc[0]
+
+    sc1_co2 = aviation_df.loc[df['fuel_type']
+                              == fuel_type, 'sc1_co2'].iloc[0]
+    sc1_ch4 = aviation_df.loc[df['fuel_type']
+                              == fuel_type, 'sc1_ch4'].iloc[0]
+    sc1_n20 = aviation_df.loc[df['fuel_type']
+                              == fuel_type, 'sc1_n20'].iloc[0]
+    sc1_sum = aviation_df.loc[df['fuel_type']
+                              == fuel_type, 'sc1_sum'].iloc[0]
+
+    CO2_e = float(Q) * EC * (sc1_co2) / 1000
+    CH4_e = float(Q) * EC * (sc1_ch4) / 1000
+    N2O_e = float(Q) * EC * (sc1_n20) / 1000
+    total_e = float(Q) * EC * (sc1_sum) / 1000
+
+    return total_e, CO2_e, CH4_e, N2O_e
